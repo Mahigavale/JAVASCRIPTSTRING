efe7ca29b7f61f93c51b4fe7c8382655bc021b8e
Mahigavale <maheshgavale07@gmail.com>
2024-02-22 17:39:35 +0530

    the compare

---
+++ compare.js
@@ -0,0 +1,17 @@
+console.log('cat' > 'Bat');
+//output: true
+
+console.log('Zebra' < 'Xerox');
+//output: true
+
+let str = 'JavaScript is a high-level,' + 
+    'often just-in-time compiled language that conforms to' + 
+    ' ECMAScript standard It has dynamic typing,'+ 
+    ' prototype-based object-orientation,' + 
+    ' and first-class functions.'
+console.log(str);
+
+let longString = "JavaScript is a high-level \
+often just-in-time compiled \
+language."
+console.log(longString);
