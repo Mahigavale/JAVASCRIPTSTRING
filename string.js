71aef77b58a88465f1c84be1b47590a7281cd5aa
Mahigavale <maheshgavale07@gmail.com>
2024-02-22 15:37:24 +0530

    string intro:

---
+++ string.js
@@ -0,0 +1,11 @@
+//string is an immutable object in the js.
+//there are two ways to create the string in js.
+//string literal and string object
+// when you create the string with the use of the new keyword then
+//it is the string object with wich you creted the string object.
+
+let str='mahesh';
+let str2=new String('mahesh');
+
+console.log(typeof(str)); //output will be string 
+console.log(typeof(str2));//out put will be object.
