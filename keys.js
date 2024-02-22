9ebf509c753ecd66cd422589efb7935fc47cfa31
Mahigavale <maheshgavale07@gmail.com>
2024-02-22 16:22:04 +0530

    the keys and values

---
+++ keys.js
@@ -0,0 +1,19 @@
+
+let names=['A','B','C','D'];
+
+let kys=names.keys();
+
+for(var v of  kys)
+{
+    console.log("the keys are:",v);
+}
+
+let vls=names.values();
+
+for( var vl of vls)
+{
+    console.log("values are:",vl);
+}
+
+console.log(v);
+console.log(vls);
