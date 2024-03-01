f82fb39fb1298afe8b9e1a6d7600f271b4e9929f
Mahigavale <maheshgavale07@gmail.com>
2024-03-01 14:26:59 +0530

    join in array

---
+++ arrayrevision/join.js
@@ -0,0 +1,12 @@
+//join is used to join the elements of the array to convert them into the
+//string.
+
+
+
+let arr=['mahesh', 'is', 'best'];
+let str=arr.join('');
+let str2=arr.join(' ');
+let str3=arr.join('-');
+console.log(str);
+console.log(str2);
+console.log(str3);
