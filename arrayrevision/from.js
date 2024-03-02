b62e2863df9faaa5108a5debcec3ca66128548c8
Mahigavale <maheshgavale07@gmail.com>
2024-03-02 14:36:24 +0530

    from method in array is used to extract the array from the given string. it exactly behaves like split but with no space.

---
+++ arrayrevision/from.js
@@ -0,0 +1,12 @@
+/*
+from method is used to extraact the array from the given 
+string.
+*/
+
+let str='mahesh ashok gavale';
+console.log(Array.from(str));
+console.log(str.split(''));
+
+// we can say that from is equal to split('').
+//remeber there isno space in the split here.
+
