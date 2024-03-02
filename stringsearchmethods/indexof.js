70e877e93c890ceda149f9de85d6704bd7af9bb9
Mahigavale <maheshgavale07@gmail.com>
2024-03-02 17:59:01 +0530

    the folder for string search methods

---
+++ stringsearchmethods/indexof.js
@@ -0,0 +1,8 @@
+/*
+the index of method returns the first occurance of the given string
+in the main string.
+if not found then it returns the -1 here.
+*/
+let str="mai yaha hoon yaha hoon yaha hoon yaha !";
+
+console.log(str.indexOf('yaha'));
