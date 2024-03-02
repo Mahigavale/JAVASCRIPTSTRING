e7664f02514dde7ad0aecff1eb32720ef0d3c4de
Mahigavale <maheshgavale07@gmail.com>
2024-03-02 18:01:36 +0530

    the index of method

--- stringsearchmethods/indexof.js
+++ stringsearchmethods/indexof.js
@@ -5,4 +5,7 @@ if not found then it returns the -1 here.
 */
 let str="mai yaha hoon yaha hoon yaha hoon yaha !";
 
+console.log(str.indexOf('y'));
 console.log(str.indexOf('yaha'));
+console.log(str.indexOf('yah'));
+console.log(str.indexOf('ya'));
