9314855195a2b2815985927fcdb7730fb03b57ce
Mahigavale <maheshgavale07@gmail.com>
2024-03-02 15:43:31 +0530

    slice updated

--- slice.js
+++ slice.js
@@ -7,3 +7,14 @@ let str2=str.slice(0,6);
 console.log(str2);
 let arr=str.split('');
 console.log(arr);
+
+/*
+if the second parameter is not provided then it will slice the
+rest of the string.
+if the negative parameter is provided then it will
+count from the end of the string.
+*/
+
+let names="mahesh ashok gavale....";
+console.log(names.slice(-1));
+console.log(names.slice(0));
