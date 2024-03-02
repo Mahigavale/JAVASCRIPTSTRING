300b48fe25ae0b3a2e162e8c22ef86bff5624cea
Mahigavale <maheshgavale07@gmail.com>
2024-03-02 15:33:17 +0530

    slice method in js.
    it takes two arguements,
    start and end (end is excluded here).
    it returns the new string and the original string is remians undaunted.

---
+++ slice.js
@@ -0,0 +1,9 @@
+let str="I am the best!";
+console.log(str.length);
+
+let str2=str.slice(0,6);
+//here 6 is not inclued in the output of the string.
+//one space is equal to one length.
+console.log(str2);
+let arr=str.split('');
+console.log(arr);
