bb74cacb76179d7f54aac1a9b7258da66fddedc7
Mahigavale <maheshgavale07@gmail.com>
2024-03-04 16:02:40 +0530

    the ends with

---
+++ stringsearchmethods/endwith.js
@@ -0,0 +1,8 @@
+/*
+this method is used to check if the given string ends with the 
+particular string.
+returns true if true else false.
+*/
+let names='mahesh ashok gavale';
+
+console.log(names.endsWith('le'));
