ac51c3201575f7e2a50d6b3900d495d1841cd299
Mahigavale <maheshgavale07@gmail.com>
2024-03-04 13:19:33 +0530

    the starts with

---
+++ stringsearchmethods/startswith.js
@@ -0,0 +1,12 @@
+//this method is used to check  if the specified string
+//is started with the specific word.
+//true returns if true else returns false.
+
+let str="I am hello from mars!";
+console.log(str.startsWith('I am h'));
+console.log(str.startsWith("i"));
+//console.log(str.startsWith(/i/g));
+/*
+starts with does not takes the regex as a argument for 
+startswith.
+*/
